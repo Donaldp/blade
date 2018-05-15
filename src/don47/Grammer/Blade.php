94a8aa8a5f187c684abb30d259dc1eeacb80c8db
Donald Pakkies <donaldpakkies@gmail.com>
2018-05-15 16:27:13 +0200

    Feat: added new control structures
    Added foreach, for, switch, isset, etc

--- src/don47/Grammer/Blade.php
+++ src/don47/Grammer/Blade.php
@@ -10,8 +10,18 @@ class Blade extends Grammer implements Fluent
   /**
    * (incomeplete)
    */
+  public static $_SWITCH = false;
+
   public function handle()
   {
+    // Control Structures
+
+    /**
+     * if
+     * elseif
+     * else
+     * endif
+     */
     $code = $this->translate('/\@\bif\b \((.*)\)/', function($match) {
       return "<?php if ($match[1]) : ?>";
     });
@@ -27,7 +37,76 @@ class Blade extends Grammer implements Fluent
     $code = $this->translate('/\@\bendif\b/', function($match) {
       return "<?php endif; ?>";
     });
-    
+
+    /**
+     * isset
+     * endisset
+     */
+    $code = $this->translate('/\@\bisset\b\((.*)\)/', function($match) {
+      return "<?php if (isset($match[1])) :?>";
+    });
+
+    $code = $this->translate('/\@\bendisset\b/', function($match) {
+      return "<?php endif; ?>";
+    });
+
+    /**
+     * switch
+     * case - case "red":
+     * break - break;
+     * default - default;
+     * endswitch
+     */
+    $code = $this->translate('/\@\bswitch\b\((.*)\)/', function($match) {
+      $this->$_SWITCH = true;
+      return "<?php switch($match[1]) :";
+    });
+
+    $code = $this->translate('/\@\bcase\b\((.*)\)/', function($match) {
+      if ($this->$_SWITCH == true) {
+        $this->$_SWITCH = false;
+        return "case $match[1] :?>";
+      }
+
+      return "<?php case $match[1] :?>";
+    });
+
+    $code = $this->translate('/\@\bbreak\b/', function($match) {
+      return "<?php break; ?>";
+    });
+
+    $code = $this->translate('/\@\bdefault\b/', function($match) {
+      return "<?php default; ?>";
+    });
+
+    $code = $this->translate('/\@\bendswitch\b/', function($match) {
+      return "<?php endswitch; ?>";
+    });
+
+    /**
+     * for
+     * endfor
+     */
+    $code = $this->translate('/\@\bfor\b \((.*)\)/', function($match) {
+      return "<?php for ($match[1]) : ?>";
+    });
+
+    $code = $this->translate('/\@\bendfor\b/', function($match) {
+      return "<?php endfor; ?>";
+    });
+
+    /**
+     * foreach
+     * endforeach
+     */
+    $code = $this->translate('/\@\bforeach\b \((.*)\)/', function($match) {
+      return "<?php foreach ($match[1]) : ?>";
+    });
+
+    $code = $this->translate('/\@\bendforeach\b/', function($match) {
+      return "<?php endforeach; ?>";
+    });
+
     return $code;
   }
 }
